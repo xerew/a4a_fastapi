173f683f70fb41fd3bfb3c20bbc1788079ddec6f
Nikolaos Antonios Grammatikos <nikantgram@gmail.com>
2025-05-22 16:28:35 +0300

    docker fixes

--- app/core/config.py
+++ app/core/config.py
@@ -1,21 +1,18 @@
-# --- MongoDB ---
-from pymongo import MongoClient
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-MONGO_URI = "mongodb://admin:adminpassword@localhost:27017/fiko?authSource=admin"
-# client = MongoClient(MONGO_URL)
-client = AsyncIOMotorClient(MONGO_URI)
-db = client["fiko"]
+# --- MongoDB ---
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://admin:adminpassword@localhost:27017/fiko?authSource=admin")
+mongo_client = AsyncIOMotorClient(MONGO_URI)
+db = mongo_client["fiko"]
 
 def get_db():
     return db
 
 # --- PostgreSQL ---
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-POSTGRES_URL = "postgresql+psycopg2://postgres:root@localhost:5432/a4a_fastapi"
-
+POSTGRES_URL = os.getenv("POSTGRES_URL", "postgresql+psycopg2://postgres:root@localhost:5432/a4a_fastapi")
 engine = create_engine(POSTGRES_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
